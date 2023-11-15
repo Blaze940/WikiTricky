97a4d8e0d4172e6a1c5b63afd3d4b74e0792ac26
Mouss <diarra940.moustapha@gmail.com>
2023-11-15 01:41:47 +0100

    Commit global structure and flavors

---
+++ lib/flavors/flavor_premium.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppConfig {
+  static const String appName = '8Heures+';
+  static const String flavorName = 'premium';
+  static const Color primaryColor = Color(0xFF620000);
+  static const bool popularityFeature = true;
+  static const bool buttonAnimation = true;
+}
+
---
+++ lib/flavors/flavor_standard.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppConfig {
+  static const String appName = '8Heures';
+  static const String flavorName = 'standard';
+  static const Color primaryColor = Color(0xFF6200EE);
+  static const bool popularityFeature = false;
+  static const bool buttonAnimation = false;
+}
