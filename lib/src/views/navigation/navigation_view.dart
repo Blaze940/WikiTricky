81a07b5a4ddbcf96588af1f6d2839217f295fd86
Mouss <diarra940.moustapha@gmail.com>
2024-02-09 03:19:01 +0100

    fix community post view

--- lib/src/views/navigation/navigation_view.dart
+++ lib/src/views/navigation/navigation_view.dart
@@ -1,46 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wiki_tricky/src/blocs/posts_bloc/post_bloc.dart';
-import 'package:wiki_tricky/src/views/post/community_post_list_view.dart';
 import 'package:wiki_tricky/src/widgets/custom_app_bar.dart';
 import 'package:wiki_tricky/src/widgets/custom_nav_bar.dart';
-import 'package:wiki_tricky/src/widgets/post_card.dart';
 
-class NavigationView extends StatefulWidget {
+import '../post/community_post_list_view.dart';
+
+class NavigationView extends StatelessWidget {
   static const String routeName = '/home';
-  final double PRELOAD_LIMIT = 0.6;
 
   const NavigationView({Key? key}) : super(key: key);
-
-  @override
-  _NavigationViewState createState() => _NavigationViewState();
-}
-
-class _NavigationViewState extends State<NavigationView> {
-  final ScrollController _scrollController = ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-    BlocProvider.of<PostBloc>(context, listen: false).add(GetItems());
-    _scrollController.addListener(_onScroll);
-  }
-
-  @override
-  void dispose() {
-    _scrollController.removeListener(_onScroll);
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  void _onScroll() {
-    if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent * widget.PRELOAD_LIMIT) {
-      final postBloc = BlocProvider.of<PostBloc>(context, listen: false);
-      if (postBloc.state.currentPost?.nextPage != null && postBloc.state.status != PostStatus.loading) {
-        postBloc.add(GetNextItems(postBloc.state.currentPost!.nextPage!));
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +16,7 @@ class _NavigationViewState extends State<NavigationView> {
         appBar: const CustomAppBar(
           titleText: 'WikiTwiki',
         ),
-        body: CommunityPostListView(scrollController: _scrollController),
+        body: const CommunityPostListView(),
         bottomNavigationBar: const CustomBottomNavBar(),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
--- lib/src/views/post/community_post_list_view.dart
+++ lib/src/views/post/community_post_list_view.dart
@@ -3,50 +3,76 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wiki_tricky/src/blocs/posts_bloc/post_bloc.dart';
 import 'package:wiki_tricky/src/widgets/post_card.dart';
 
-class CommunityPostListView extends StatelessWidget {
-  final ScrollController scrollController;
+class CommunityPostListView extends StatefulWidget {
+  const CommunityPostListView({Key? key}) : super(key: key);
 
-  const CommunityPostListView({Key? key, required this.scrollController}) : super(key: key);
+  @override
+  State<CommunityPostListView> createState() => _CommunityPostListViewState();
+}
+
+class _CommunityPostListViewState extends State<CommunityPostListView> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _loadInitialPosts();
+  }
+
+  void _loadInitialPosts() {
+    BlocProvider.of<PostBloc>(context, listen: false).add(GetItems());
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent * 0.6) {
+      final postBloc = BlocProvider.of<PostBloc>(context, listen: false);
+      if (postBloc.state.currentPost?.nextPage != null && postBloc.state.status != PostStatus.loading) {
+        postBloc.add(GetNextItems(postBloc.state.currentPost!.nextPage!));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
       onRefresh: () async {
-        BlocProvider.of<PostBloc>(context).add(GetItems()); // Rafraîchir les éléments
+        BlocProvider.of<PostBloc>(context, listen: false).add(GetItems());
       },
       child: BlocBuilder<PostBloc, PostState>(
         builder: (context, state) {
           final items = state.items;
           if (items.isEmpty && state.status == PostStatus.loading) {
-            // Si vous voulez afficher le chargement initial sans le RefreshIndicator
-            return SingleChildScrollView(
-              physics: AlwaysScrollableScrollPhysics(),
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                child: const Center(
-                  child: CircularProgressIndicator(color: Color(0xFF8B0000)),
-                ),
-              ),
+            return ListView.builder(
+              itemCount: 1,
+              itemBuilder: (context, index) => const Center(child: CircularProgressIndicator()),
+            );
+          } else if (items.isEmpty && state.status == PostStatus.success) {
+            return ListView(
+              children: const [Center(child: Text("No post yet"))],
+            );
+          } else {
+            return ListView.builder(
+              controller: _scrollController,
+              itemCount: items.length + (state.currentPost?.nextPage != null ? 1 : 0),
+              itemBuilder: (context, index) {
+                if (index < items.length) {
+                  return PostCard(item: items[index]);
+                } else {
+                  return state.currentPost?.nextPage != null
+                      ? const Center(child: CircularProgressIndicator())
+                      : Container(); // Retourne un conteneur vide si pas de nextPage
+                }
+              },
             );
           }
-          if (items.isEmpty && state.status == PostStatus.success) {
-            return const Center(child: Text("No post yet"));
-          }
-          final isLoadingNextPage = state.status == PostStatus.loading && items.isNotEmpty;
-
-          return ListView.builder(
-            controller: scrollController,
-            itemCount: items.length + (isLoadingNextPage ? 1 : 0),
-            itemBuilder: (context, index) {
-              if (index < items.length) {
-                final item = items[index];
-                return PostCard(item: item);
-              } else {
-
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          );
         },
       ),
     );
